ac2703db20cd9995ee5a4115c38a21921a85433e
azrdev123 <dev19dev99@gmail.com>
2026-06-10 20:57:06 +0100

    Add join requests API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\ChampionshipController;
 use App\Http\Controllers\Api\FakePaymentController;
+use App\Http\Controllers\Api\JoinRequestController;
 use App\Http\Controllers\Api\PlayerController;
 use App\Http\Controllers\Api\SeasonController;
 use App\Http\Controllers\Api\TeamController;
@@ -12,6 +13,11 @@ Route::apiResource('championships', ChampionshipController::class);
 Route::get('fake-payments', [FakePaymentController::class, 'index']);
 Route::post('fake-payments', [FakePaymentController::class, 'store']);
 Route::get('fake-payments/{fakePayment}', [FakePaymentController::class, 'show']);
+Route::get('join-requests', [JoinRequestController::class, 'index']);
+Route::post('join-requests', [JoinRequestController::class, 'store']);
+Route::get('join-requests/{joinRequest}', [JoinRequestController::class, 'show']);
+Route::put('join-requests/{joinRequest}/accept', [JoinRequestController::class, 'accept']);
+Route::put('join-requests/{joinRequest}/refuse', [JoinRequestController::class, 'refuse']);
 Route::apiResource('players', PlayerController::class);
 Route::apiResource('seasons', SeasonController::class);
 Route::apiResource('teams', TeamController::class);
