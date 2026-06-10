5db578109c35c9907b0e5fe466dba237602103a8
azrdev123 <dev19dev99@gmail.com>
2026-06-10 19:20:27 +0100

    Add teams CRUD API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\SeasonController;
+use App\Http\Controllers\Api\TeamController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('seasons', SeasonController::class);
+Route::apiResource('teams', TeamController::class);
