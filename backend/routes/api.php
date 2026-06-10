1f658b1b718c1450ef65dc3e47fe077ac2bd2ce1
azrdev123 <dev19dev99@gmail.com>
2026-06-10 20:51:17 +0100

    Add fake payment API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,18 @@
 <?php
 
 use App\Http\Controllers\Api\ChampionshipController;
+use App\Http\Controllers\Api\FakePaymentController;
 use App\Http\Controllers\Api\PlayerController;
 use App\Http\Controllers\Api\SeasonController;
 use App\Http\Controllers\Api\TeamController;
+use App\Http\Controllers\Api\TournamentController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('championships', ChampionshipController::class);
+Route::get('fake-payments', [FakePaymentController::class, 'index']);
+Route::post('fake-payments', [FakePaymentController::class, 'store']);
+Route::get('fake-payments/{fakePayment}', [FakePaymentController::class, 'show']);
 Route::apiResource('players', PlayerController::class);
 Route::apiResource('seasons', SeasonController::class);
 Route::apiResource('teams', TeamController::class);
+Route::apiResource('tournaments', TournamentController::class);
