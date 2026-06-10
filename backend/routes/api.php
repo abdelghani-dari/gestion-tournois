e71c625d2055a036a5829fb0f5ba0e54237cfe4b
azrdev123 <dev19dev99@gmail.com>
2026-06-10 19:35:45 +0100

    Add championships CRUD API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\ChampionshipController;
 use App\Http\Controllers\Api\PlayerController;
 use App\Http\Controllers\Api\SeasonController;
 use App\Http\Controllers\Api\TeamController;
 use Illuminate\Support\Facades\Route;
 
+Route::apiResource('championships', ChampionshipController::class);
 Route::apiResource('players', PlayerController::class);
 Route::apiResource('seasons', SeasonController::class);
 Route::apiResource('teams', TeamController::class);
